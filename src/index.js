d054866ebbbf245eac65a2627bee32930a8bf8b8
rogatty <igor@wikia-inc.com>
2017-11-18 17:49:00 +0100

    Use reactjs/redux/todomvc as the base

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import './style';
-import App from './components/app';
+import "./index.css";
 
-export default App;
+import React from "react";
+import { render } from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./containers/App";
+import reducer from "./reducers";
+
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
